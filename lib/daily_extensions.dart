34aadc8cf68dfcb877981c02179b294e4c867519
Mary <info@phimary.com>
2022-07-17 12:22:02 +0300

    Initial commit

---
+++ lib/daily_extensions.dart
@@ -0,0 +1,9 @@
+library daily_extensions;
+
+export 'src/double_extensions.dart';
+export 'src/int_extensions.dart';
+export 'src/list_extensions.dart';
+export 'src/map_extensions.dart';
+export 'src/num_extensions.dart';
+export 'src/set_extensions.dart';
+export 'src/string_extensions.dart';
